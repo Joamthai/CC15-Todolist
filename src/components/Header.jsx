93882efb2a13942781013afc66a13a783a180d21
Joamthai <joamthai@gmail.com>
2023-08-29 17:11:48 +0700

    Restructuring

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import './Header.scss';
-import { FaHome, FaSearch } from 'react-icons/fa';
+import { FaHome } from 'react-icons/fa';
+import Search from './Search'
 
 function Header(){
     return ( <header className="header">
@@ -7,15 +8,10 @@ function Header(){
       <FaHome />
     </div>
     <div className="header__text">
-      <h3>Todoist</h3>
+      <h1>Todoist</h1>
     </div>
     <div className="header__search">
-      <div className='search'>
-        <span className='search__icon'>
-            <FaSearch />
-        </span>
-        <input type="text" placeholder='search' className='search__input' />
-      </div>
+      <Search />
     </div>
   </header>)
 }
