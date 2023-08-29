af3ea64e2e4676e79e368173b065c486c4bd372b
Joamthai <joamthai@gmail.com>
2023-08-29 16:19:09 +0700

    finish:header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import './App.scss';
+import { FaHome } from 'react-icons/fa';
+
 function App() {
-  return <div>React-App</div>;
+  return (
+    <div className="todo">
+      <div className="todo__header">
+        <header className="header">
+          <div className="header__logo">
+            <FaHome />
+          </div>
+          <div className="header__text">
+            <h3>Todoist</h3>
+          </div>
+          <div className="header__search">
+            <input />
+          </div>
+        </header>
+      </div>
+      <div className="todo__sidebar">SideBar</div>
+      <div classNAme="todo__content">TodoContent</div>
+    </div>
+  );
 }
 
 export default App;
