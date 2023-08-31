22ad20c58274da88731cfc7b0c6f2ae9e35c8f06
Joamthai <joamthai@gmail.com>
2023-08-31 15:17:02 +0700

    finish:todoform

--- src/components/Todo/TodoCreate.jsx
+++ src/components/Todo/TodoCreate.jsx
@@ -2,37 +2,79 @@ import { useState } from 'react';
 import { FaPlus } from 'react-icons/fa';
 import { HiPlus } from 'react-icons/hi';
 
-import TodoForm from './TodoForm'
+import TodoForm from './TodoForm';
 import styles from './TodoCreate.module.scss';
 
-// Concept#1 : true ? <Add Task/> : <TodoFrom/>
-// Concept#2 : onClick : FN will be run after clicked
-// Concept#3 : Js Value can't make React Rerender, must use State
-// Concept#3 : React state
-//      const [state, setState] = useState(initial:any)
-//        element 1 : current State
-//        element 2 : FN for State
+/* 
+CC1 - Condition Rendering
+- Default : Show Button & Text
+- Active : Show TodoForm
 
-// #1 : Function Component (Render)
+ Concept : true ? <AddTask/> : <TodoForm/>
+*/
+
+/* 
+CC2 - EVENT HANDLING
+- เอาฟังก์ชันไปผูกติดกับ UI เพื่อให้ USER เป็นคนเรียกใช้ฟังก์ชันเอง
+- onClick : ต้อง Click ก่อน , FN ถึงจะรัน
+    - User ทำการคลิก
+    - Browser จะเป็นคนเรียกใช้ฟังก์ชัน โดยส่ง parameter มา 1 ตัว
+      handleClick(eventObject)
+*/
+
+/* 
+CC3 - JS Value ไม่สามารถทำให้ React Rerender ได้
+ต้องใช้ State
+*/
+
+/*
+CC-4 Array Destructuring
+function myUseState() {
+  return [5,9]
+}
+let  [a,b] = myUseState()
+a === 5
+b === 9
+
+function myUseState(initial) {
+  return [initial,9]
+}
+let  [a,b] = myUseState(5)
+a === 5
+b === 9
+*/
+
+/* 
+CC5 - React State (1 ในฟังก์ชันของกลุ่ม React Hook)
+  const [state,setState] = useState(initialState:any)
+  // element 1: current State
+  // element 2 : Fn สำหรับ SetState
+  // เมื่อ State เปลี่ยน Function Component จะ Rerender
+  // Rerender 1 ครั้ง == Code ทั้งหมดใน FC จะถูกรันใหม่ 1 ครั้ง
+*/
+
 function TodoCreate() {
-  // HOOK FN
-  const [isOpenForm, setIsOpenForm] = useState(false)
+  const [isOpenForm, setIsOpenForm] = useState(false);
 
-  // #2 : JS Function (Logic)
-  const handleClick = function(event) {
-    setIsOpenForm(!isOpenForm)
-  }
+  const handleClick = function () {
+    setIsOpenForm(!isOpenForm);
+  };
 
   return (
     <>
-    {isOpenForm ?  <TodoForm textSubmit='Add Task'/> :
-    (<div className={styles.todo__create} onClick={handleClick}>
-      <div className={styles.todo__create__button}>
-        <HiPlus />
-      </div>
-      <h3 className={styles.todo__create__text}>Add Task</h3>
-    </div>)
-    }
+      {isOpenForm ? (
+        <TodoForm 
+          textSubmit='Add Task' 
+          setIsOpenForm={setIsOpenForm}
+        />
+      ) : (
+        <div className={styles.todo__create} onClick={handleClick}>
+          <div className={styles.todo__create__button}>
+            <HiPlus />
+          </div>
+          <h3 className={styles.todo__create__text}>Add Task</h3>
+        </div>
+      )}
     </>
   );
 }
--- src/components/Todo/TodoForm.jsx
+++ src/components/Todo/TodoForm.jsx
@@ -1,17 +1,66 @@
+import { useState } from 'react';
+import { Button } from '../Common/Button/Button';
 import styles from './TodoForm.module.scss';
+/*
+  props = {
+    textSubmit : string
+  }
+*/
+/*
+CC1- Form Handle
+- ใช้ FN ไปผูกกับ Event ชื่อ onSubmit
+- FN จะถูก Browserเรียกใช้ (เมื่อไหร่ ?) โดยส่ง parameter มา 1 ตัว (event Object)
+- โดย default ทุกปุ่มใน <form> จะทำหน้าที่ submit
+- วิธีแก้ ต้องกำหนด type ของปุ่ม
+  - type="submit" :  <button type='button'>1</button>
+  - type="button" :  <button type='submit'>2</button>
+*/
 
+/* 
+props = {
+  textSubmit : string
+  setIsOpenForm : FN
+}
+*/
 function TodoForm(props) {
+
+  const [isError, setIsError] = useState(false)
+  const [input, setInput] = useState('')
+  const [taskInput , setTaskInput] = useState('')
+
+  const handleChangeInput =function(event){
+    if(isError) setIsError(false)
+    setTaskInput(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if(taskInput.trim()=== ''){
+      console.log('error')
+      setIsError(true)
+      return
+    } 
+      console.log('submit')
+    
+  }
+
+  const handleCancel = () => {
+    props.setIsOpenForm(false)
+  }
+
   return (
-    <form className={styles.todo__form__container}>
-      {/*	Body */}
-      <input className={styles.todo__form__input} placeholder='Task Name' />
+    <form 
+    className={styles.todo__form__container} onSubmit={handleSubmit}>
+      {/* Body */}
+      <input className={styles.todo__form__input} placeholder='Task Name' value={taskInput} onChange={handleChangeInput}/>
 
       {/*Form Footer */}
       <div className={styles.todo__form__footer}>
-        <p className={styles.todo__error}>Title is required</p>
+        {isError ? <p className={styles.todo__error}>Title is required</p> : null}
         <div className={styles.todo__form__buttons}>
-          <button text='Cancel' active={false}/>
-          <button text={props.textSubmit} active={true}/>
+          <Button text='Cancel' active={false} type='button'onClick={handleCancel}/>
+          <Button text={props.textSubmit} active={true} type='submit'/>
         </div>
       </div>
     </form>
