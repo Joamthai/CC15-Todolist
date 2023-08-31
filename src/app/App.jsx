6cfd1f0c9f314d62c815cc44798cf78b443973aa
Joamthai <joamthai@gmail.com>
2023-08-31 09:37:36 +0700

    finish:UI

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,54 +1,26 @@
+// Dependencies
 import './App.scss';
-import Header from '../components/Header';
-import {FaInbox,FaCalendar,FaCalendarAlt,FaChevronDown} from 'react-icons/fa';
-
-import List from '../components/List';
+import AppBar from '../components/Common/AppBar/AppBar';
+import SideBar from '../components/SideBar/SideBar';
 import TodoHeader from '../components/Todo/TodoHeader';
 import TodoCreate from '../components/Todo/TodoCreate';
-import TodoList from '../components/Todo/TodoList';
-// import Listitem from './Listitem';
+import TodoLists from '../components/Todo/TodoLists';
+import { Button } from '../components/Common/Button/Button';
 
 function App() {
-
-  const generalList = [
-    {id:1, text:'Inbox', icon:<FaInbox/>, active:true},
-    {id:2, text:'Today', icon:<FaCalendar/>, active:false},
-    {id:3, text:'Next 7 days', icon:<FaCalendarAlt/>, active:false},
-  ];
-  const projectList = [
-    {id:4, text:'Project-A', icon:<FaInbox/>, active:true},
-    {id:5, text:'Project-B', icon:<FaInbox/>, active:false},
-  ];
-
   return (
-    <div className="todo">
-      <div className="todo__header">
-        <Header />
+    <div className='todo'>
+      <div className='todo__header'>
+        <AppBar />
       </div>
-      <div className="todo__sidebar">
-        <aside className="sidebar">
-          <section className="sidebar_category">
-            <List data={generalList}/>
-          </section>
-          <section className="sidebar_category">
-            <div className='accordion'>
-              <div className='accordion__toggle'></div>
-              <li className='accordion__item'>
-                <FaChevronDown className='accordion__item__icon accordion__item__active'/>
-                <p className='accordion__item__text'>Projects</p>
-              </li>
-              <List data={projectList}/>
-            </div>
-          </section>
-        </aside>
+      <div className='todo__sidebar'>
+        <SideBar />
       </div>
-      <div className="todo__content">
+      <div className='todo__content'>
         <main className='todo__container'>
-          <TodoHeader/>
-          <TodoCreate/>
-          <TodoList/>
-          
-        
+          <TodoHeader />
+          <TodoCreate />
+          <TodoLists />
         </main>
       </div>
     </div>
@@ -56,12 +28,3 @@ function App() {
 }
 
 export default App;
-
-
-/*
-Challenge : Refactor
-- ให้ 2 section render UI ที่...
-- OptionA (2/5) : render ต่างกัน <List/> กับ <Accordion/>
-- OptionB (4/5) : render UI เดียวกัน เช่น <List/>
-- OptionC (5/5) : render <List/> ภายใต้ <Accordion><List/></Accordion>
-*/
